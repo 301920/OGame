f2f4dad80f4cee60e111c52257d9c678dce02ed7
Shurukn Thirty-Two <280471737+301920@users.noreply.github.com>
2026-08-20 16:07:39 -0400

    Meta file created

---
+++ Shurukn Ultimate Expeditions.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Ogame - Shurukn's Ultimate Expedtions
+// @namespace    https://github.com/301920/OGame
+// @version      0.1
+// @description  Shurukn's Ultimate Expeditions for OGame (Gameforge). Auto-divide / EXPEDITION / Last Fleet, Fast Mode (Admiral + STANDARD galaxy preset), escort (Reaper/BC/Destroyer), origin lock, delayed start, delay between expeditions, recovery on return legs, continuous smart loop. OGLight + AntiGameReborn aware.
+// @author       Shurukn
+// @match        https://*.ogame.gameforge.com/game/*
+// @icon         https://i.ibb.co/9H9MvwgS/image.png
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_notification
+// @run-at       document-idle
+// @downloadURL  https://github.com/301920/OGame/Shurukn Ultimate Expeditions.user.js
+// @updateURL    https://github.com/301920/OGame/Shurukn Ultimate Expeditions.meta.js
+// ==/UserScript==
